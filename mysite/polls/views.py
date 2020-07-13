d72749262a043f5f0242f38891ee7d54c65e7ffe
Farrukhbek <farruhzokirov00@gmail.com>
2020-07-13 15:28:04 +0500

    poll app fully functioning

---
+++ mysite/polls/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    return render(request, 'polls/index.html', {
+        'latest_question_list': latest_question_list,
+    })
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/detail.html', {
+        'question': question,
+    })
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't choose a choice!"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {
+        'question': question
+    })
